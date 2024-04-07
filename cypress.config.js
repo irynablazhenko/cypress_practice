734f289319e16182c20a221860e9cf59ec106d80
Iryna Blazhenko <iryna.blazhenko@gmail.com>
2024-04-07 18:09:47 +0300

    Changed configuration file and added HW 12

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://the-internet.herokuapp.com',
+    baseUrl: 'https://qauto.forstudy.space',
   },
 });
 
